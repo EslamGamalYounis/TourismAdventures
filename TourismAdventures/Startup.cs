ec73bea07b0f69e65003130535aa48b874247e5f
Ahmed Elbassal <Ahmed.Elbassal@linkdev.com>
2022-06-16 01:08:37 +0200

    make conn string

--- TourismAdventures/Startup.cs
+++ TourismAdventures/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +26,10 @@ namespace TourismAdventures
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+
+            services.AddDbContext<DbContext>(options =>
+            options.UseSqlServer(this.Configuration.GetConnectionString("dbConn")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
